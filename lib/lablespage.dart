f3f217ef175f25d2fc46a1ec582406491cd8aaf0
Aakash Shelke <shelkeaakash2@gmail.com>
2021-07-16 16:18:01 +0530

    Labels added on clicked image background

--- lib/lablespage.dart
+++ lib/lablespage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
+import 'package:popover/popover.dart';
+
 class LabelsPage extends StatelessWidget {
   File image;
 
@@ -30,85 +32,138 @@ class LabelsPage extends StatelessWidget {
               filter: new ImageFilter.blur(sigmaX: 1.5, sigmaY: 1.5),
               child: new Container(
                 decoration:
-                    new BoxDecoration(color: Colors.grey.withOpacity(0.3)),
+                new BoxDecoration(color: Colors.grey.withOpacity(0.3)),
               ),
             ),
           ),
-          Positioned.fill(child: Container()),//map
-
-          Positioned(
-              top: 0,
-              child: MaterialButton(
-                onPressed: () {},
-                minWidth: 25.0,
-                color: Colors.black,
-                // padding: EdgeInsets.symmetric(
-                //   vertical: 12.0,
-                // ),
-                child: Text(
-                  "Top",
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.white,
-                  ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    const Button(),
+                    const Button(),
+                    const Button(),
+                  ],
                 ),
-              )
-          ),
-          Positioned(
-              left: 10,
-              child: MaterialButton(
-                onPressed: () {},
-                minWidth: 25.0,
-                color: Colors.black,
-                // padding: EdgeInsets.symmetric(
-                //   vertical: 12.0,
-                // ),
-                child: Text(
-                  "Left",
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.white,
-                  ),
-                ),
-              )
-          ),
-          Positioned(
-              right: 10,
-              child: MaterialButton(
-                onPressed: () {},
-                minWidth: 25.0,
-                color: Colors.black,
-                // padding: EdgeInsets.symmetric(
-                //   vertical: 12.0,
-                // ),
-                child: Text(
-                  "Right",
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.white,
-                  ),
-                ),
-              )
-          ),
-          Positioned(
-              bottom: 0,
-              child: MaterialButton(
-                color: Colors.black,
-                onPressed: () {},
-                minWidth: 25.0,
-                // padding: EdgeInsets.symmetric(
-                //   vertical: 12.0,
-                // ),
-                child: Text(
-                  "Bottom",
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.white,
-                  ),
-                ),
-              )
+              ],
+            ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  const Button({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80,
+      height: 40,
+      decoration: const BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        boxShadow: [BoxShadow(color: Colors.transparent, blurRadius: 5)],
+      ),
+      child: GestureDetector(
+        child: const Center(child: Text('Label', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),)),
+        onTap: () {
+          showPopover(
+            context: context,
+            transitionDuration: const Duration(milliseconds: 150),
+            bodyBuilder: (context) => const ListItems(),
+            onPop: () => print('Popover was popped!'),
+            direction: PopoverDirection.bottom,
+            width: 200,
+            height: 400,
+            arrowHeight: 15,
+            arrowWidth: 30,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ListItems extends StatelessWidget {
+  const ListItems({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.of(context)
+                  ..pop()
+                  ..push(
+                    MaterialPageRoute<SecondRoute>(
+                      builder: (context) => SecondRoute(),
+                    ),
+                  );
+              },
+              child: Container(
+                height: 50,
+                color: Colors.black,
+                child: const Center(child: Text('Machine Stats', style: TextStyle(color: Colors.amber),)),
+              ),
+            ),
+            const Divider(thickness: 20, color: Colors.white),
+            Container(
+              height: 50,
+              color: Colors.black,
+              child: const Center(child: Text('Total Uptime : 23.5 minutes', style: TextStyle(color: Colors.amber),)),
+            ),
+            const Divider(),
+            Container(
+              height: 50,
+              color: Colors.black,
+              child: const Center(child: Text('Total Downtime : 0', style: TextStyle(color: Colors.amber),)),
+            ),
+            const Divider(),
+            Container(
+              height: 50,
+              color: Colors.black,
+              child: const Center(child: Text('Last Maint : 15-07-2021', style: TextStyle(color: Colors.amber),)),
+            ),
+            const Divider(),
+            Container(
+              height: 50,
+              color: Colors.black,
+              child: const Center(child: Text('Addition Info.', style: TextStyle(color: Colors.amber),)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Go back!'),
+        ),
       ),
     );
   }
